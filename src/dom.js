68243f410c741204a1c99194fb2fb3097894e0da
Stelios Kiayias <s.kiayias@gmail.com>
2017-05-01 15:21:17 +0100

    Remove 'debugger' statement

--- src/dom.js
+++ src/dom.js
@@ -6,34 +6,33 @@
  * @param  {String|Array|Object} children The children of DOM node.
  * @return {Function}            DOM node.
  */
-const initialise = (type = 'div', [attributes = {}, children = null]) => {
-  children = Array.isArray(children) ? children : [children]
+const initialise = (type = 'div', [attributes = ({}, (children = null))]) => {
+  children = Array.isArray(children) ? children : [children];
 
   // create an empty DOM node.
-  const node = document.createElement(type)
+  const node = document.createElement(type);
 
-  debugger
   // add attributes.
-  Object.keys(attributes).forEach(name => node.setAttribute(name, attributes[name]))
+  Object.keys(attributes).forEach(name => node.setAttribute(name, attributes[name]));
 
   // loop over all children and append them to the newly created DOM node.
   return children.reduce((prev, cur) => {
     if (cur != null) {
       if (typeof cur === 'string' || typeof cur === 'number') {
-        const textNode = document.createTextNode(cur + '')
-        prev.appendChild(textNode)
-        return prev
+        const textNode = document.createTextNode(cur + '');
+        prev.appendChild(textNode);
+        return prev;
       }
 
       // 'cur' in this case is an HTML (DOM like) object
       if (typeof cur === 'object') {
-        prev.appendChild(cur)
-        return prev
+        prev.appendChild(cur);
+        return prev;
       }
     }
-    return prev
-  }, node)
-}
+    return prev;
+  }, node);
+};
 
 /**
  * createNode - Creates a simple DOM node.
@@ -47,23 +46,26 @@ const createNode = (type, options) => content =>
   // For example you could do: DOM.tr().$text('tr sample').append(td('td sample'))... etc.
   Object.assign(initialise(type, content, options), {
     $text(textContent) {
-      _node.textContent(textContent)
-      return _node
+      _node.textContent(textContent);
+      return _node;
     },
 
     $append(node) {
-      return _node.appendChild(node)
+      return _node.appendChild(node);
     },
 
     // useful method for chaining.
     $appendTo(node) {
-      _node.appendChild(node)
-      return _node
-    }
-  })
+      _node.appendChild(node);
+      return _node;
+    },
+  });
 
-export default new Proxy({}, {
-  get(attrs, type) {
-    return options => createNode(type, options)
-  }
-})
+export default new Proxy(
+  {},
+  {
+    get(attrs, type) {
+      return options => createNode(type, options);
+    },
+  },
+);
